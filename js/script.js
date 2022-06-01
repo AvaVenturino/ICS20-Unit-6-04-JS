2271d772de5a57c2423d35c8817a192e21b609fa
Ava Venturino <99355031+AvaVenturino@users.noreply.github.com>
2022-06-01 16:47:52 +0000

    made multiple changes

--- js/script.js
+++ js/script.js
@@ -13,39 +13,18 @@ if (navigator.serviceWorker) {
   });
 }
 
-/**
- * Get API info.
- */
-// code from: https://www.youtube.com/watch?v=670f71LTWpM
+window.onload = function() {
+  // this calculates volume of a sphere
 
-/**
- * Get API info.
- */
-const getWeather = async (URLAddress) => {
-  try {
-    const request = await fetch(URLAddress);
-    const jsonData = await request.json();
-    var tempK = jsonData.main.temp;
-    var tempC = 0;
-    const feeling = jsonData.weather[0];
-    const image = feeling.icon;
+  const params = new URLSearchParams(document.location.search)
 
-    console.log(jsonData.weather);
-    document.getElementById("api-image").innerHTML =
-      "<img src='http://openweathermap.org/img/wn/" +
-      image +
-      "@2x.png' alt='Weather Icon' width='10%'><br><h5>";
-    (">");
-
-    // Calculate from Kalvin to Celsius
-    tempC = tempK - 273.15;
-
-    document.getElementById("api-weather").innerHTML =
-      "The current weather is " + tempC.toFixed(2) + " °C";
-  } catch (err) {
-    console.log(err);
-  }
-};
-getWeather(
-  "https://api.openweathermap.org/data/2.5/weather?lat=45.4211435&lon=-75.6900574&appid=fe1d80e1e103cff8c6afd190cad23fa5"
-);
+  // input
+  const radius = params.get('r')
+  // process
+  const volume = 4/3 * Math.PI * Math.pow(radius, 3)
+  const dimensions = "<ul>\n<li>Radius = " + radius + "</li>\n</ul>"
+  // output
+  document.getElementById('dimensions').innerHTML = dimensions
+  document.getElementById("volume").innerHTML =
+    "The volume is: " + volume.toFixed(2) + ' cm³' 
+}
