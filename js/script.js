7d2605db1765b92d8faba96adcd29bcab3398580
Ava Venturino <99355031+AvaVenturino@users.noreply.github.com>
2022-06-01 17:21:49 +0000

    changed pwa stuff

--- js/script.js
+++ js/script.js
@@ -8,8 +8,8 @@
  * Check servie worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS20-Unit-6-03-JS/sw.js", {
-    scope: "/ICS20-Unit-6-03-JS/",
+  navigator.serviceWorker.register("/ICS20-Unit-6-04-JS/sw.js", {
+    scope: "/ICS20-Unit-6-04-JS/",
   });
 }
 
--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-var GHPATH = "/ICS20-Unit-6-03-JS";
+var GHPATH = "/ICS20-Unit-6-04-JS";
 var APP_PREFIX = "gppwa_";
 var VERSION = "version_001";
-var GHPATH = "/ICS20-Unit-6-03-JS";
+var GHPATH = "/ICS20-Unit-6-04-JS";
 var URLS = [
   `${GHPATH}/`,
   `${GHPATH}/index.html`,
